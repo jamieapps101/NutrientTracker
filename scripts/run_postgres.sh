cd9e897ec6434144a218ca050f83c8e51c15179f
jamie <jamieapps101@gmail.com>
2022-07-23 15:34:53 +0100

    postgres example container script bugfix

--- scripts/run_postgres.sh
+++ scripts/run_postgres.sh
@@ -1,7 +1,6 @@
 #! /usr/bin/env bash
 
-
-## sets up postgres instance in foregroumd
+## sets up example postgres instance in foregroumd
 
 docker run \
     --interactive \
@@ -9,5 +8,6 @@ docker run \
     --env POSTGRES_PASSWORD=example_password \
     --name  postgres_inst \
     --rm \
-    --expose 127.0.0.1:8080:8080 \
+    --expose 8080 \
+    --network="host" \
     postgres:14.3-bullseye
